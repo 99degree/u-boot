d263c20532fcca5c85697529731d8e04957e7e6c
Vladimir Zapolskiy <vladimir.zapolskiy@linaro.org>
2023-04-27 11:13:37 +0100

    rb2: boot from emmc
    Signed-off-by: Vladimir Zapolskiy <vladimir.zapolskiy@linaro.org>

--- include/configs/qrb4210-rb2.h
+++ include/configs/qrb4210-rb2.h
@@ -15,7 +15,19 @@
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	"bootm_size=0x5000000\0"	\
-	"bootm_low=0x80000000\0"	\
-	"bootcmd=bootm $prevbl_initrd_start_addr\0"
+	"bootm_low=0x90008000\0"	\
+	"bootcmd=mmc dev 0; "	\
+	"setenv fdt_high 0xffffffffffffffff; "	\
+	"setenv fdt_addr_r 0x80008000; "	\
+	"ext4load mmc 0:0xe ${fdt_addr_r} /boot/qrb4210-rb2.dtb; "	\
+	"setenv kernel_addr_r 0x80100000; "	\
+	"ext4load mmc 0:0xe ${kernel_addr_r} /boot/Image.gz; "	\
+	"setenv kernel_comp_size ${filesize}; "	\
+	"setenv kernel_comp_addr_r 0x41000000; "	\
+	"setenv initrd_high 0xffffffffffffffff; "	\
+	"setenv ramdisk_addr_r 0x83000000; "	\
+	"ext4load mmc 0:0xe ${ramdisk_addr_r} /boot/initramfs-rbX.cpio.gz; "	\
+	"setenv bootargs console=ttyMSM0,115200n8 root=/dev/mmcblk1p2 rootwait rw ignore_loglevel earlycon; "	\
+	"booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}\0"
 
 #endif
