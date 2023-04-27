bcefac546e47bc812f22b823bfd28b3213a2509a
Vladimir Zapolskiy <vladimir.zapolskiy@linaro.org>
2023-04-27 06:44:00 +0100

    arm: snapdragon: qcm2290: update boot command
    Signed-off-by: Vladimir Zapolskiy <vladimir.zapolskiy@linaro.org>

--- include/configs/rb1.h
+++ include/configs/rb1.h
@@ -14,6 +14,18 @@
 #define CFG_EXTRA_ENV_SETTINGS \
 	"bootm_size=0x5000000\0"	\
 	"bootm_low=0x90008000\0"	\
-	"bootcmd=bootm $prevbl_initrd_start_addr\0"
+	"bootcmd=mmc dev 1; "	\
+	"setenv fdt_high 0xffffffffffffffff; "	\
+	"setenv fdt_addr_r 0x80008000; "	\
+	"ext4load mmc 1:0x2 ${fdt_addr_r} /boot/qrb2210-rb1.dtb; "	\
+	"setenv kernel_addr_r 0x80100000; "	\
+	"ext4load mmc 1:0x2 ${kernel_addr_r} /boot/Image.gz; "	\
+	"setenv kernel_comp_size ${filesize}; "	\
+	"setenv kernel_comp_addr_r 0x41000000; "	\
+	"setenv initrd_high 0xffffffffffffffff; "	\
+	"setenv ramdisk_addr_r 0x83000000; "	\
+	"ext4load mmc 1:0x2 ${ramdisk_addr_r} /boot/initramfs-rbX.cpio.gz; "	\
+	"setenv bootargs console=ttyMSM0,115200n8 root=/dev/mmcblk1p2 rootwait rw ignore_loglevel earlycon; "	\
+	"booti ${kernel_addr_r} ${ramdisk_addr_r}:${filesize} ${fdt_addr_r}\0"
 
 #endif
