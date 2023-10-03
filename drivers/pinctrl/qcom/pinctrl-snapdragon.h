8727cfee6369e8da731fec21c26f9f041628e40a
Caleb Connolly <caleb.connolly@linaro.org>
2023-10-03 11:58:44 +0100

    pinctrl: qcom: add sm6115 pinctrl driver
    This SoC features a pinctrl block with west, east, and south tiles.
    
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- drivers/pinctrl/qcom/pinctrl-snapdragon.h
+++ drivers/pinctrl/qcom/pinctrl-snapdragon.h
@@ -34,5 +34,6 @@ extern struct msm_pinctrl_data apq8096_data;
 extern struct msm_pinctrl_data sdm845_data;
 extern struct msm_pinctrl_data qcs404_data;
 extern struct msm_pinctrl_data qcm2290_data;
+extern struct msm_pinctrl_data sm6115_data;
 
 #endif
