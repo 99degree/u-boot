369e54e2f64ddd0072f1925e23f5f16cfffdfc17
Caleb Connolly <caleb.connolly@linaro.org>
2023-10-23 16:16:37 +0100

    HACKING: configs: qcom: disable autoboot
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

--- include/configs/qcom-common.h
+++ include/configs/qcom-common.h
@@ -14,12 +14,11 @@
 #define CFG_SYS_BAUDRATE_TABLE	{ 115200, 230400, 460800, 921600, 3000000 }
 
 #define CFG_EXTRA_ENV_SETTINGS \
-	"bootdelay=1\0" \
+	"bootdelay=-1\0" \
 	"bootmenu_delay=-1\0" \
 	"stdin=serial,button-kbd\0"	\
 	"stdout=vidconsole,serial\0"	\
 	"stderr=vidconsole,serial\0" \
-	"preboot=bootflow scan -l\0" \
 	"bootmenu_0=Boot first available device=bootflow scan -b\0" \
 	"bootmenu_1=Enable USB mass storage=ums 0 scsi 0,1,2,3,4,5\0" \
 	"bootmenu_2=Enable fastboot mode=fastboot usb 0\0" \
