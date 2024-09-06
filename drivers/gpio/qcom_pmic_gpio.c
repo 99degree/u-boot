Android-Boot-Manager/droidboot_device_generic_u-boot

gpio/qcom_pmic: add pm7325
Add a compatible for pm7325 PMIC

Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

drivers/gpio/qcom_pmic_gpio.c
@@ -322,6 +322,7 @@ static const struct udevice_id qcom_gpio_ids[] = {
 	{ .compatible = "qcom,pm8994-gpio" },	/* 22 GPIO's */
 	{ .compatible = "qcom,pm8998-gpio", .data = PMIC_MATCH_READONLY },
 	{ .compatible = "qcom,pms405-gpio" },
+	{ .compatible = "qcom,pm7325-gpio", .data = PMIC_MATCH_READONLY },
 	{ }
 };
 

99degree/u-boot

drivers/gpio/qcom_pmic_gpio.c
@@ -366,6 +366,7 @@ static const struct udevice_id qcom_gpio_ids[] = {
 	{ .compatible = "qcom,pm6125-gpio", .data = QCOM_PMIC_QUIRK_READONLY },
 	{ .compatible = "qcom,pm8150-gpio", .data = QCOM_PMIC_QUIRK_READONLY },
 	{ .compatible = "qcom,pm8550-gpio", .data = QCOM_PMIC_QUIRK_READONLY },
+	{ .compatible = "qcom,pm7325-gpio", .data = QCOM_PMIC_QUIRK_READONLY },
 	{ }
 };
 