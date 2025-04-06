fcd29fe24f28a7cd5dd6bd0f0b2063b044a30f99
George Chan <gchan9527@gmail.com>
2025-04-06 22:42:23 +0800

    [DO_NOT_MERGE] Introduce some scripts for build/boot
    Signed-off-by: George Chan <gchan9527@gmail.com>

---
+++ build_output.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+if [ -e board/qualcomm/misc.config ]
+then
+	echo ok found misc.config
+else
+	echo generate misc.config
+	echo CONFIG_PANIC_HANG=n > board/qualcomm/misc.config
+fi
+
+if [ -e .output ]
+then
+	echo ok
+else
+	ln -sf /tmp/output ./.output
+fi
+
+rm -rf /tmp/output
+
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- O=/tmp/output/ qcom_defconfig qcom-phone.config qcom-sm71xx.config qcom-multi-dtb.config misc.config
+make ARCH=arm CROSS_COMPILE=aarch64-linux-gnu- O=/tmp/output/ -j$(nproc)
+
+gzip /tmp/output/u-boot-nodtb.bin -c > /tmp/output/u-boot-nodtb.bin.gz
+cat /tmp/output/u-boot-nodtb.bin.gz /tmp/output/dts/upstream/src/arm64/qcom/qcom-multi-dummy.dtb > /tmp/output/uboot-dtb
+./mkbootimg/mkbootimg --base '0x0' --kernel_offset '0x00008000' --pagesize '4096' --kernel /tmp/output/uboot-dtb --ramdisk /tmp/output/fit-dtb.blob -o /tmp/output/u-boot.img
